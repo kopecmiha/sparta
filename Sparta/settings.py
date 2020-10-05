11b2c8f9bd09d27ce3c1dc8f1a1f483317be4603
Михаил <koptseff.mikhail@yandex.ru>
2020-10-05 19:30:56 +0300

    "some commit"

--- Sparta/settings.py
+++ Sparta/settings.py
@@ -127,4 +127,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOWED_ORIGINS = [
+ "https://learn-front.tw1.ru",
+ "http://127.0.0.1:8000",
+]
